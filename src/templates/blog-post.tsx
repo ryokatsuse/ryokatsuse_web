2fd687c58b0da2f95e3fbc3197ed8a29db9a3dcb
ryokatsuse <ryokatsu0719@gmail.com>
2020-02-04 11:12:49 +0900

    feat: snsアイコンの表示

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -4,12 +4,18 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Helmet from "react-helmet"
+import {
+  TwitterShareButton,
+  TwitterIcon
+} from 'react-share';
 
 class BlogPostTemplate extends React.Component<any, any> {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
     const { previous, next } = this.props.pageContext
+    const url = typeof window !== 'undefined' ? window.location.href : ''
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -33,6 +39,29 @@ class BlogPostTemplate extends React.Component<any, any> {
           <section dangerouslySetInnerHTML={{ __html: post.html }} />
           <hr/>
           <footer>
+            <div className="sns-btn">
+              <TwitterShareButton title={post.frontmatter.title} url={url} className="mr-2">
+              <TwitterIcon size={40} round />
+              </TwitterShareButton>
+              <Helmet>
+              <script type="text/javascript" src="//b.st-hatena.com/js/bookmark_button.js" charset="utf-8" async="async" />
+              </Helmet>
+              <a
+                href="http://b.hatena.ne.jp/entry/"
+                className="hatena-bookmark-button"
+                data-hatena-bookmark-layout="vertical-normal"
+                data-hatena-bookmark-lang="ja"
+                title="このエントリーをはてなブックマークに追加"
+                >
+                <img
+                  src="//b.st-hatena.com/images/entry-button/button-only@2x.png"
+                  alt="このエントリーをはてなブックマークに追加"
+                  width="20"
+                  height="20"
+                  style={{border: 'none'}}
+                />
+              </a>
+            </div>
             <Bio />
           </footer>
         </article>
