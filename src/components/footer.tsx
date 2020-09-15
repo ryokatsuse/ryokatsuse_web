8afd52656b91010bf3232b55205c79d8b0c8ba66
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-09-15 12:23:33 +0900

    feat: footerをstyled-components化した

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,64 @@
 import * as React from "react"
+import styled from "styled-components"
 
-const Footer : React.FC<any> = () => {
+type Props = {
+  className?: string
+}
+
+const FCFooter : React.FC<Props> = ({className}) => {
   return (
-    <div>
-        <footer>
-          <hr />
-          <div className="links">
-            <a href="https://github.com/ryokatsuse"
-              rel="nofollow noopener noreferrer"
-              target="_blank">GitHub</a>
-            <a 
-              href="https://twitter.com/ryo__kts"
-              rel="nofollow noopener noreferrer"
-              target="_blank">Twitter</a>
-          </div>
-          <p className="copy-right">
-            © {new Date().getFullYear()}
-            {` `}
-            <a href="/">Ryo Kastuse Blog</a>
-          </p>
-          <p>Google Analyticsを使っています。</p>
-        </footer>
+    <footer className={`${className}`}>
+      <hr />
+      <div className={`${className}__links`}>
+        <a className={`${className}__links-link`}
+          href="https://github.com/ryokatsuse"
+          rel="nofollow noopener noreferrer"
+          target="_blank">GitHub</a>
+        <a className={`${className}__links-link`}
+          href="https://twitter.com/ryo__kts"
+          rel="nofollow noopener noreferrer"
+          target="_blank">Twitter</a>
       </div>
+      <p className={`${className}__copy-right`}>
+        © {new Date().getFullYear()}
+        {` `}
+        <a href="/">Ryo Kastuse Blog</a>
+      </p>
+      <p>Google Analyticsを使っています。</p>
+    </footer>
   );
 };
-export default Footer
+export const Footer = styled(FCFooter)`
+  padding: var(--grid);
+  text-align: center;
+  padding: 0;
+
+  &__links {
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &-link {
+      color: #333;
+      margin-right: 8px;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
+
+  &__copy-right {
+    font-size: 1rem;
+    font-style: italic;
+  }
+  @media (prefers-color-scheme: dark) {
+    &__links {
+      &-link {
+        color: #fff;
+      }
+    }
+  }
+
+`
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Header } from "../components/header"
-import Footer from "../components/footer"
+import { Footer } from "../components/footer"
 
 const Layout : React.FC<any> = ({children}) => {
   return (
