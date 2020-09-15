99f3513173373bd16716ed0635b74952dcdc31da
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-09-15 12:05:51 +0900

    feat:headerをstyled-components化した

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Header from "../components/header"
+import { Header } from "../components/header"
 import Footer from "../components/footer"
 
 const Layout : React.FC<any> = ({children}) => {
