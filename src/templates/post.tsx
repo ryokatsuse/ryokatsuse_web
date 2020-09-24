357fab6152a2b2eb19898094774c0b279d012bde
Ryo Katsuse <ryokatsu0719@gmail.com>
2020-09-24 15:25:52 +0900

    fix:記事ページのリファクタリング

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -17,20 +17,18 @@ const FCPost: React.FC<Props> = ({ className, post, slug }) => {
   const url = typeof window !== 'undefined' ? window.location.href : ''
 
   return (
-    <article className="blog-post">
+    <article className={`${className}`}>
       <header>
-        <h1>
+        <h1 className={`${className}__title`}>
           {post.frontmatter.title}
         </h1>
-        <p
-          style={{
-            display: `block`
-          }}
-        >
+        <p>
           {post.frontmatter.date}
         </p>
       </header>
-      <section dangerouslySetInnerHTML={{ __html: post.html }} />
+      <section
+        className={`${className}__section`}
+        dangerouslySetInnerHTML={{ __html: post.html }} />
       <hr/>
       <ShareButtons text={post.frontmatter.title} url={url} />
     </article>
@@ -38,5 +36,42 @@ const FCPost: React.FC<Props> = ({ className, post, slug }) => {
 }
 
 export const Post = styled(FCPost)`
-  
+
+  p {
+    margin-bottom: 1.75rem;
+  }
+
+  .gatsby-resp-image-wrapper {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  & h1,
+  & h2,
+  & h3,
+  & h4 {
+    margin: 1.5rem 0;
+  }
+
+  & h2 {
+    font-size: 1.8rem;
+  }
+
+  & h3 {
+    font-size: 1.6rem;
+  }
+
+  & h4 {
+    font-size: 1.4rem;
+  }
+
+  &__section {
+    margin: 1rem 0;
+  }
+
+  pre[class*="language-"] {
+    padding: 12px;
+  }
+
+
 `
