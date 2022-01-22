21847a20a7fb40ac982b91802e01256a542e9a72
ryokatsu <ryokatsu0719@gmail.com>
2022-01-23 01:44:36 +0900

    スタイルの微調整 (#42)
    * fix:リファクタリング
    
    * fix:リファクタリング
    
    * fix:目次とアンカーリンクを追加
    
    * fix:TOCを追加
    
    * fix:console.log削除
    
    * fix:エラー解消
    
    * fix:不要なcssを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import { Layout } from "../components/layout"
 import {
@@ -6,7 +6,6 @@ import {
   BlogItemData
 } from "../components/BlogItem"
 import SEO from "../components/seo"
-import styled from "styled-components"
 
 type Props = {
   data: {
@@ -22,7 +21,7 @@ const BlogIndex: React.FC<Props> = ({data}) => {
   const posts = data.allMarkdownRemark.edges
 
   return (
-    <Layout location="" title="">
+    <Layout title="">
       <SEO title="ryokatsu.dev" />
       {posts.map(({ node }) => {
         return <BlogItem key={node.fields.slug} {...node} />
