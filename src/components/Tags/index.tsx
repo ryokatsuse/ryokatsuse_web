b55a38e06f5368ac048e04f791f27be369052d90
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-02-07 01:33:06 +0900

    fix:TagsコンポーネントのCSSを追加

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -2,25 +2,24 @@ import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Link } from "gatsby"
 
-import styled from "styled-components"
 
 type Props = {
-  className?: string
   tags: string[]
 }
 
-const FCTags = React.memo<Props>(({ className, tags }) => {
+export const Tags = React.memo<Props>(({ tags }) => {
   return (
-    <div className={`${className}`}>
-      <ul className={`${className}__nav`}>
+    <div className="flex items-center ml-2">
+      <ul className="flex items-center gap-2 list-none">
         {tags.map(tag => (
           <li
-          className={`${className}__nav-list`}
+          className="flex items-center gap-1"
             key={tag}>
             <FontAwesomeIcon
-              className={`${className}__nav-icon`}
+              className="text-blue-500"
               aria-label="タグ" icon={["fas", "hashtag"]} />
             <Link
+              className="text-blue-500"
               to={`/tag/${tag}`}>{tag}</Link>
           </li>
         ))}
@@ -29,30 +28,4 @@ const FCTags = React.memo<Props>(({ className, tags }) => {
   )
 })
 
-export const Tags = styled(FCTags)`
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-
-  &__nav {
-    margin: 0 0 0 10px;
-    display: flex;
-    flex-flow: row wrap;
-    align-items: center;
-    list-style: none;
-
-    &-list {
-      margin-left: 10px;
-      display: flex;
-      align-items: center;
-
-      a {
-        margin-left: 5px;
-      }
-    }
-    &-icon {
-      color: #007acc;
-    }
-  }
-`
 
