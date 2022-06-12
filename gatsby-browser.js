bb14fc6efadddd00c00b7ead8c5f0bcbe9b50b46
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-06-13 01:13:39 +0900

    fix:node-sassの削除

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 // custom typefaces
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./src/styles/prism-vsc-dark-plus.css"
-import "./src/styles/global.scss"
+import "./src/styles/global.css"
