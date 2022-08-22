0aa5cb9a05abbf94f6efae4fba0a3c60ca62db21
ryokatsu <ryokatsu0719@gmail.com>
2022-08-22 15:21:20 +0900

    fix:微調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
