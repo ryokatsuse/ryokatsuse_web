903d7503c2c363461ed4dc34f6170a421c56362b
ryokatsu <ryokatsu0719@gmail.com>
2023-03-26 16:32:26 +0900

    fix:rss

--- src/pages/posts-feed.xml.js
+++ src/pages/posts-feed.xml.js
@@ -1,18 +1,20 @@
 import rss from '@astrojs/rss';
 import dayjs from '../lib/dayjs';
+import { getCollection } from 'astro:content';
 
-const postResult = import.meta.glob('./blog/*/*.md', { eager: true });
-const posts = Object.values(postResult);
-posts.sort((a, b) => dayjs(b.frontmatter.publishDate).tz().valueOf() - dayjs(a.frontmatter.publishDate).tz().valueOf());
+export async function get(context) {
+  const allBlogEntries = (await getCollection('blog')).sort(
+    (a, b) => dayjs(b.data.publishDate).tz().valueOf() - dayjs(a.data.publishDate).tz().valueOf(),
+  );
 
-export const get = () =>
-  rss({
+  return rss({
     title: 'ryokatsu.dev',
     description: '雑記やエンジニアリングの記事',
-    site: import.meta.env.SITE,
-    items: posts.map(post => ({
-      link: post.url,
-      title: post.frontmatter.title,
-      pubDate: dayjs(post.frontmatter.publishDate).tz().toDate(),
+    site: context.site,
+    items: allBlogEntries.map(entry => ({
+      link: new URL(`/blog/${entry.slug}`, import.meta.env.SITE).toString(),
+      title: entry.data.title,
+      pubDate: entry.data.publishDate,
     })),
   });
+}
