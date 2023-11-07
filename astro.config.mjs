d87eca145da1022647e32f66545e181bebb4af6f
Ryo Katsuse <ryokatsu0719@gmail.com>
2023-11-07 19:02:14 +0900

    Update/astro v3 and pnpm (#51)
    * fix:astro3 and pnpm
    
    * fix:ワークフローの削除
    
    * fix:エラー
    
    * fix:ogp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ryokatsu.dev/', // this line is required
+  site: 'https://ryokatsu.dev',
   integrations: [
-    image(),
-    mdx(),
     sitemap(),
     tailwind({
       config: {
