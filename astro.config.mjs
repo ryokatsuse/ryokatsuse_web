5f091560c6840af721f465810c3f44bcbb2996f2
ryokatsu <ryokatsu0719@gmail.com>
2023-11-07 18:58:37 +0900

    fix:エラー

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
@@ -8,7 +7,6 @@ import react from '@astrojs/react';
 export default defineConfig({
   site: 'https://ryokatsu.dev',
   integrations: [
-    mdx(),
     sitemap(),
     tailwind({
       config: {
